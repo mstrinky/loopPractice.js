afebde39286a0b6384968401281e9003b915ac2d
Mst Rinky Akter <mstrinkya@gmail.com>
2023-02-05 23:01:38 +0600

    prblm1 finish

---
+++ prblm1.js
@@ -0,0 +1,15 @@
+for (i=1;i<=100;i++){
+    //  console.log(i);
+    if( i%3===0 && i%5===0){
+        console.log('FIZZBUZZ');
+    }
+    else if(i%5===0){
+        console.log('BUZZ');
+    }
+    else if(i%3===0){
+        console.log('FIZZ');
+    }
+    else{
+        console.log(i);
+    }
+}
