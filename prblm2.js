6b3cf859e32a8deabdf87dce0a1a4999925710e1
Mst Rinky Akter <mstrinkya@gmail.com>
2023-02-07 23:16:16 +0600

    solved

--- prblm2.js
+++ prblm2.js
@@ -1,9 +1,12 @@
 for(i=0;i<=15;i++){
     // console.log(i);
-    if(i%2===0){
+    if(i==0){
+        console.log(i + " " + 'is even');
+    }
+    else if (i%2===0){
         console.log( i+" " + "is even");
     }
-    else if(i%2!=0){
+    else {
         console.log(i+ " " + "is odd");
     }
 }
